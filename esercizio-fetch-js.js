dd8f787b564b131c34c7a2c1eb626cb48994f126
Ruben Sgarra <ruben.sgarra@libero.it>
2021-02-25 14:08:57 +0100

    esercizio fetch

---
+++ esercizio-fetch-js.js
@@ -0,0 +1,28 @@
+let button=document.getElementById("cliccami");
+let nome=document.getElementById("nome");
+let altezza=document.getElementById("altezza");
+let sesso=document.getElementById("sesso");
+let anno=document.getElementById("anno");
+let skin=document.getElementById("skin");
+
+function getData(){
+    let random=Math.floor((Math.random()*61)+1);
+    let api='https://swapi.dev/api/people/' + random;
+
+    fetch(api).then(response => {
+    return response.json();
+}).then(people =>{
+    nome.innerText=people.name;
+    anno.innerText=people.birth_year;
+    altezza.innerText=people.height;
+    sesso.innerText=people.gender;
+    skin.innerText=people.skin_color;
+
+    })
+
+
+}
+
+
+
+cliccami.addEventListener("click", getData)
