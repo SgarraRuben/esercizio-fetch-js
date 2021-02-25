2896419c9f743d1aee233132f0e2370af6b7b77c
Ruben Sgarra <ruben.sgarra@libero.it>
2021-02-25 15:00:45 +0100

    esercizio esercizio

--- esercizio-fetch-js.js
+++ esercizio-fetch-js.js
@@ -1,26 +1,26 @@
-let button=document.getElementById("cliccami");
-let nome=document.getElementById("nome");
-let altezza=document.getElementById("altezza");
-let sesso=document.getElementById("sesso");
-let anno=document.getElementById("anno");
-let skin=document.getElementById("skin");
+let button = document.getElementById("cliccami");
+let nome = document.getElementById("nome");
+let altezza = document.getElementById("altezza");
+let sesso = document.getElementById("sesso");
+let anno = document.getElementById("anno");
+let skin = document.getElementById("skin");
 
-function getData(){
-    let random=Math.floor((Math.random()*61)+1);
-    let a=document.getElementById("InputUser").value;
+function getData() {
+    // let random=Math.floor((Math.random()*61)+1);
+    let a = document.getElementById("InputUser").value;
 
-    let c=parseInt(a);
-    let api='https://swapi.dev/api/people/' + c;
+    let c = parseInt(a);
+    let api = 'https://swapi.dev/api/people/' + c;
     console.log(random);
 
     fetch(api).then(response => {
-    return response.json();
-}).then(people =>{
-    nome.innerText=people.name;
-    anno.innerText=people.birth_year;
-    altezza.innerText=people.height;
-    sesso.innerText=people.gender;
-    skin.innerText=people.skin_color;
+        return response.json();
+    }).then(people => {
+        nome.innerText = people.name;
+        anno.innerText = people.birth_year;
+        altezza.innerText = people.height;
+        sesso.innerText = people.gender;
+        skin.innerText = people.skin_color;
 
     })
 
